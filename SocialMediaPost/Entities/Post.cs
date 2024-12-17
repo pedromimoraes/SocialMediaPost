eadca7b04b846c8f21746ed027ff44d3e7aa22f2
Pedro Moraes <pedrohmirandamoraess@gmail.com>
2024-12-17 13:15:25 -0300

    Finished main class code and replaced Comment.RemoveContact() method name to Comment.RemoveComment()

--- SocialMediaPost/Entities/Post.cs
+++ SocialMediaPost/Entities/Post.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace SocialMediaPost.Entities
 {
     class Post
@@ -6,12 +8,37 @@ namespace SocialMediaPost.Entities
         public string Title { get; private set; }
         public string Content { get; private set; }
         public int Likes { get; private set; }
-        public List<Comment> Comment { get; private set; } = new List<Comment>();
+        public List<Comment> Comments { get; private set; } = new List<Comment>();
         public Post(DateTime moment, string title, string content, int likes)
         {
-
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
         }
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(Title);
+            builder.Append(Likes);
+            builder.Append(" Likes - ");
+            builder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            builder.AppendLine(Content);
+            builder.AppendLine("Comments:");
 
-
+            foreach (Comment comment in Comments)
+            {
+                builder.AppendLine(comment.Text);
+            }
+            return builder.ToString();
+        }
     }
 }
--- SocialMediaPost/Program.cs
+++ SocialMediaPost/Program.cs
@@ -1,7 +1,31 @@
-﻿namespace SocialMediaPost
+﻿using SocialMediaPost.Entities;
+using System.Globalization;
+using System.Net.NetworkInformation;
+
+namespace SocialMediaPost
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Beautiful place!");
+            Comment c2 = new Comment("Have a nice trip");
+            Comment c3 = new Comment("I really love this place!");
+
+            Post p1 = new Post(
+                DateTime.ParseExact("10/07/2024 18:33:41","dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture),
+                "Traveling to Brazil",
+                "I'm going to visit Rio de Janeiro in Brazil!",
+                25
+            );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+            p1.AddComment(c3);
+
+            System.Console.WriteLine(p1);
+        }
     }
 }
+
+
